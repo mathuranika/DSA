967f1780d45654ba491d5ae1721fbdbe22259cd9
Anika Mathur <111272336+mathuranika@users.noreply.github.com>
2025-08-26 15:52:31 +0530

    Create LCA.py
    https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-bst/1

---
+++ LCA.py
@@ -0,0 +1,18 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+class Solution:
+    def LCA(self, root, n1, n2):
+        if not root:
+            return
+        if n1.data > root.data and n2.data> root.data:
+            return self.LCA(root.right, n1, n2)
+        elif n1.data < root.data and n2.data < root.data:
+            return self.LCA(root.left, n1, n2)
+        else:
+            return root
+    
