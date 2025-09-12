a1c2952655582955292171283369dd1878cd1815
Anika Mathur <111272336+mathuranika@users.noreply.github.com>
2025-09-13 00:26:47 +0530

    Optimize 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -17,3 +17,35 @@ class Solution:
                 seen.append(nums[j])
 
         return [list(triplet) for triplet in res]
+
+#O(nlogn) Optimized
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        nums.sort()
+
+        for i in range(0,n):
+            if i>0 and nums[i] == nums[i-1]: 
+                continue
+            j = i+1
+            k = n-1
+            while j<k:
+                Sum = nums[i]+nums[j]+nums[k]
+                if Sum > 0:
+                    k-=1
+                elif Sum < 0:
+                    j+=1
+                else:
+                    res.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    k -= 1
+                    while j<k and nums[j] == nums[j-1]:
+                        j += 1
+                    while j<k and nums[k] == nums[k+1]:
+                        k -= 1
+        return res
+
+
+
+
