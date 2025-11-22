6ce976ac1fb639e3e9d2daf3481ada03d559fc29
Anika Mathur <111272336+mathuranika@users.noreply.github.com>
2025-11-22 09:49:32 +0530

    Implement next permutation algorithm in Python

--- next_permutation.py
+++ next_permutation.py
@@ -25,3 +25,39 @@ class Solution:
             arr[fix_ind + 1:] = sorted(arr[fix_ind + 1:])
 
         return arr
+
+
+## Leetcode
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        piv = None
+
+        for i in range(n-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                piv = i
+                break
+
+        if piv is None:
+            nums.reverse()
+            return nums
+
+        min_ind = piv + 1
+        for j in range(piv+1, n):
+            if nums[j] > nums[piv] and nums[j] <= nums[min_ind]:
+                min_ind = j
+
+        nums[piv], nums[min_ind] = nums[min_ind], nums[piv]
+
+        arr_sort = nums[piv+1:]
+        arr_sort.sort()
+
+        k = piv + 1
+        l = 0
+        while k < n:
+            nums[k] = arr_sort[l]
+            k += 1
+            l += 1
+
+        return nums
